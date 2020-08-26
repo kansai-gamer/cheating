0f2e9c9ff09f3b55b8d0dc3b4e7b4b4cf670d04c
kansai-gamer <2004230001at@edu.oca.ac.jp>
2020-08-27 06:27:40 +0900

    0827

---
+++ 偶数.php
@@ -0,0 +1,7 @@
+<?php
+$total = 0;
+for ($i = 0; $i <= 9; $i = $i + 2){
+  $total = $total + $i;
+}
+echo $total;
+?>
---
+++ 奇数.php
@@ -0,0 +1,7 @@
+<?php
+$total = 0;
+for ($i = 1; $i <= 9; $i = $i + 2){
+  $total = $total + $i;
+}
+echo $total;
+?>
